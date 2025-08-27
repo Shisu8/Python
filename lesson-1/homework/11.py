1c176f2cd3ff8fa3280d4be6f5832f463db36a05
Shisu8 <inboxmail89@mail.ru>
2025-08-27 19:43:15 +0500

    Add files via upload

---
+++ lesson-1/homework/11.py
@@ -0,0 +1,23 @@
+#Given a side of square. Find its perimeter and area
+
+a = input("Choose your number ")
+print ("Perimeter is ",int(a)*4, "and area is ", int(a)**2)
+
+#Given diameter of circle. Find its length.
+
+import math
+a = input("Choose your number ")
+print ("Length of a circle is ", float(a)*math.pi*2)
+#Given two numbers a and b. Find their mean.
+
+a=int(input("Select your number a "))
+b=int(input("Select your number b "))
+print((a+b)/2)
+
+#Given two numbers a and b. Find their sum, product and square of each number.
+
+a=int(input("Select your number a "))
+b=int(input("Select your number b "))
+print("Sum = ", a+b)
+print("Products = ", a*b)
+print("Squares A=",a**2, "Squares B=",b**2 )
